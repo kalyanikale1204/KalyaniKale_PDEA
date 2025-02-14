b43eea15822c34e4261321cce36368bd7267db33
kalyanikale1204 <kale.kalyani1204@gmail.com>
2025-02-13 23:12:35 -0800

    updating file

---
+++ src/main/java/Utilities/TestBase.java
@@ -0,0 +1,34 @@
+package Utilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class TestBase {
+	public static String hubURL = "https://hub.lambdatest.com/wd/hub";
+    
+	public synchronized void setUp(String browser) throws MalformedURLException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", browser);
+        capabilities.setCapability("browserVersion", "112");
+        HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("user", "kale.kalyani1204");
+        ltOptions.put("accessKey", "ZdcL9V7lOiEzRyR7OHFl3Ljr0a6bAIrnSw77pZJIJBMwpK3xjN");
+        ltOptions.put("build", "LambdaTestCertification_SeleniumJava101");
+        ltOptions.put("name", this.getClass().getName());
+        ltOptions.put("platformName", "Windows 10");
+        ltOptions.put("seCdp", true);
+        ltOptions.put("selenium_version", "4.0.0");
+        capabilities.setCapability("LT:Options", ltOptions);
+
+        DriverFactory.getInstances().setDriver(new RemoteWebDriver(new URL(hubURL), capabilities));
+	}
+	
+	public WebDriver driver() {
+		return DriverFactory.getInstances().getDriver();
+	}
+}
